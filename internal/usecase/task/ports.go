aec9a9dee7ff76c4add5f4070ce0c76d3426d242
cxii112 <lyakhovoy.d@ya.ru>
2026-04-14 10:43:44 +0500

    массовое сохранение задач

--- internal/usecase/task/ports.go
+++ internal/usecase/task/ports.go
@@ -13,6 +13,7 @@ type Repository interface {
 	Update(ctx context.Context, task *taskdomain.Task) (*taskdomain.Task, error)
 	Delete(ctx context.Context, id int64) error
 	List(ctx context.Context) ([]taskdomain.Task, error)
+	CreateBulk(ctx context.Context, tasks []taskdomain.Task) ([]taskdomain.Task, error)
 }
 
 type Usecase interface {
